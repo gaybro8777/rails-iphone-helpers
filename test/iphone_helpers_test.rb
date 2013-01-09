af832f37fe9245b921b66e60c987527524857099
Anton Lindqvist <anton@qvister.se>
2013-01-09 15:11:50 +0100

    Added custom assertion method.

--- test/iphone_helpers_test.rb
+++ test/iphone_helpers_test.rb
@@ -2,46 +2,46 @@ class IphoneHelpersTest < MiniTest::Unit::TestCase
   include ActionView::Helpers::TagHelper
 
   def test_apple_mobile_web_app_capable
-    assert_equal iphone(:fullscreen), '<meta content="yes" name="apple-mobile-web-app-capable" />'
+    assert_tag iphone(:fullscreen), :meta, name: 'apple-mobile-web-app-capable', content: 'yes'
   end
 
   def test_apple_mobile_web_app_status_bar_style
-    assert_equal iphone(:status_bar), '<meta content="default" name="apple-mobile-web-app-status-bar-style" />'
+    assert_tag iphone(:status_bar), :meta, name: 'apple-mobile-web-app-status-bar-style', content: 'default'
   end
 
   def test_apple_mobile_web_app_status_bar_style_black
-    assert_equal iphone(:status_bar, :color => 'black'), '<meta content="black" name="apple-mobile-web-app-status-bar-style" />'
+    assert_tag iphone(:status_bar, color: 'black'), :meta, name: 'apple-mobile-web-app-status-bar-style', content: 'black'
   end
 
   def test_apple_mobile_web_app_status_bar_style_black_translucent
-    assert_equal iphone(:status_bar, :color => 'black-translucent'), '<meta content="black-translucent" name="apple-mobile-web-app-status-bar-style" />'
+    assert_tag iphone(:status_bar, color: 'black-translucent'), :meta, name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent'
   end
 
   def test_apple_touch_icon
-    assert_equal iphone(:icon, :href => '/assets/iphone.png'), '<link href="/assets/iphone.png" rel="apple-touch-icon" />'
+    assert_tag iphone(:icon, href: '/assets/iphone.png'), :link, rel: 'apple-touch-icon', href: '/assets/iphone.png'
   end
 
   def test_apple_touch_icon_precomposed
-    assert_equal iphone(:icon, :href => '/assets/iphone.png', :precomposed => true), '<link href="/assets/iphone.png" rel="apple-touch-icon-precomposed" />'
+    assert_tag iphone(:icon, href: '/assets/iphone.png', precomposed: true), :link, rel: 'apple-touch-icon-precomposed', href: '/assets/iphone.png'
   end
 
   def test_apple_touch_startup_image
-    assert_equal iphone(:splash, :href => '/assets/splash.png'), '<link href="/assets/splash.png" rel="apple-touch-startup-image" />'
+    assert_tag iphone(:splash, href: '/assets/splash.png'), :link, rel: 'apple-touch-startup-image', href: '/assets/splash.png'
   end
 
   def test_format_detection_disable_telephone
-    assert_equal iphone(:format_detection, :telephone => false), '<meta content="telephone=no" name="format-detection" />'
+    assert_tag iphone(:format_detection, telephone: false), :meta, name: 'format-detection', content: 'telephone=no'
   end
 
   def test_format_detection_enable_telephone
-    assert_equal iphone(:format_detection, :telephone => true), '<meta content="telephone=yes" name="format-detection" />'
+    assert_tag iphone(:format_detection, telephone: true), :meta, name: 'format-detection', content: 'telephone=yes'
   end
 
   def test_viewport
-    assert_equal iphone(:viewport), '<meta content="initial-scale=1; maximum-scale=1; width=device-width" name="viewport" />'
+    assert_tag iphone(:viewport), :meta, name: 'viewport', content: 'initial-scale=1; maximum-scale=1; width=device-width'
   end
 
   def test_viewport_with_custom_attributes
-    assert_equal iphone(:viewport, :width => 320, :height => 480, :initial_scale => 2.5, :minimum_scale => 0.5, :maximum_scale => 3, :user_scalable => 'no'), '<meta content="height=480; initial-scale=2.5; maximum-scale=3; minimum-scale=0.5; user-scalable=no; width=320" name="viewport" />'
+    assert_tag iphone(:viewport, width: 320, height: 480, initial_scale: 2.5, minimum_scale: 0.5, maximum_scale: 3, user_scalable: 'no'), :meta, name: 'viewport', content: 'height=480; initial-scale=2.5; maximum-scale=3; minimum-scale=0.5; user-scalable=no; width=320'
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,3 +5,12 @@ require 'awesome_print'
 require 'minitest/autorun'
 require 'action_view'
 require File.join(File.dirname(__FILE__), '../lib/iphone_helpers')
+
+module MiniTest::Assertions
+  def assert_tag(tag, name, attributes = {})
+    document = HTML::Document.new(tag, false, false)
+    options = {tag: name.to_s, attributes: attributes}
+
+    assert !document.find(options).nil?
+  end
+end
