9d5a725e7bda6af9728664de48f785c004d3d059
Anton Lindqvist <anton@qvister.se>
2013-01-09 15:23:56 +0100

    Added blank lines.

--- lib/rails_iphone_helpers.rb
+++ lib/rails_iphone_helpers.rb
@@ -13,12 +13,14 @@ module RailsIphoneHelpers
     options[:name] = 'format-detection'
     options[:content] = "telephone=#{(options[:telephone] && 'yes') || 'no'}"
     options[:telephone] = nil
+
     [:meta, options]
   end
 
   def fullscreen(options)
     options[:name] = 'apple-mobile-web-app-capable'
     options[:content] = 'yes'
+
     [:meta, options]
   end
 
@@ -26,11 +28,13 @@ module RailsIphoneHelpers
     options[:rel] = 'apple-touch-icon'
     options[:rel] += '-precomposed' if options[:precomposed]
     options[:precomposed] = nil if options[:precomposed]
+
     [:link, options]
   end
 
   def splash(options)
     options[:rel] = 'apple-touch-startup-image'
+
     [:link, options]
   end
 
@@ -38,6 +42,7 @@ module RailsIphoneHelpers
     options[:name] = 'apple-mobile-web-app-status-bar-style'
     options[:content] = (%w[black black-translucent].include?(options[:color]) && options[:color]) || 'default'
     options[:color] = nil if options[:color]
+
     [:meta, options]
   end
 
@@ -51,6 +56,7 @@ module RailsIphoneHelpers
     defaults = {:'initial-scale' => 1, :'maximum-scale' => 1, :width => 'device-width'}
     content = defaults.merge(options).stringify_keys.sort.collect { |k,v| "#{k}=#{v}" }.join(', ')
     options = {:content => content, :name => 'viewport'}
+
     [:meta, options]
   end
 end
