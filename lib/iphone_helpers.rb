4f2862bf7de5f4bda479487c77a4c4b8d00a69d2
Anton Lindqvist <anton@qvister.se>
2013-01-09 16:49:44 +0100

    Minor refactoring.

--- lib/iphone_helpers.rb
+++ lib/iphone_helpers.rb
@@ -1,6 +1,8 @@
 require 'action_view'
 
 module IphoneHelpers
+  STATUS_BAR_COLORS = %w[black black-translucent]
+
   def iphone(key, options = {})
     tagname, attributes = send(key, options)
 
@@ -11,8 +13,8 @@ module IphoneHelpers
 
   def format_detection(options)
     options[:name] = 'format-detection'
-    options[:content] = "telephone=#{(options[:telephone] && 'yes') || 'no'}"
-    options[:telephone] = nil
+    options[:content] = 'telephone='
+    options[:content] += options.delete(:telephone) ? 'yes' : 'no'
 
     [:meta, options]
   end
@@ -26,8 +28,7 @@ module IphoneHelpers
 
   def icon(options)
     options[:rel] = 'apple-touch-icon'
-    options[:rel] += '-precomposed' if options[:precomposed]
-    options[:precomposed] = nil if options[:precomposed]
+    options[:rel] += '-precomposed' if options.delete(:precomposed)
 
     [:link, options]
   end
@@ -39,23 +40,25 @@ module IphoneHelpers
   end
 
   def status_bar(options)
+    color = options.delete(:color)
     options[:name] = 'apple-mobile-web-app-status-bar-style'
-    options[:content] = (%w[black black-translucent].include?(options[:color]) && options[:color]) || 'default'
-    options[:color] = nil if options[:color]
+    options[:content] = STATUS_BAR_COLORS.include?(color) ? color : 'default'
 
     [:meta, options]
   end
 
   def viewport(options)
+    default_options = {
+      'initial-scale' => 1,'maximum-scale' => 1, 'width' => 'device-width'
+    }
     options = options.map do |key, value|
-      key = key.to_s.gsub('_', '-').to_sym
+      key = key.to_s.gsub('_', '-')
 
       [key, value]
     end
     options = Hash[options]
-    defaults = {:'initial-scale' => 1, :'maximum-scale' => 1, :width => 'device-width'}
-    content = defaults.merge(options).stringify_keys.sort.collect { |k,v| "#{k}=#{v}" }.join(', ')
-    options = {:content => content, :name => 'viewport'}
+    content = default_options.merge(options).sort.collect { |k, v| "#{k}=#{v}" }.join(', ')
+    options = {content: content, name: 'viewport'}
 
     [:meta, options]
   end
