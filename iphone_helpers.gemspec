d97baa16486ddcbce9369c2618dd628b7ac40568
Anton Lindqvist <anton@qvister.se>
2013-06-27 21:09:55 +0200

    Added Rails 4.0.0 support.

--- iphone_helpers.gemspec
+++ iphone_helpers.gemspec
@@ -17,6 +17,6 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'awesome_print', '~> 1.1.0'
   s.add_development_dependency 'coveralls', '~> 0.6.0'
   s.add_development_dependency 'minitest', '~> 4.4.0'
-  s.add_development_dependency 'rails', '~> 3.2.0'
+  s.add_development_dependency 'rails', '~> 4.0.0'
   s.add_development_dependency 'rake', '~> 10.0.0'
 end
--- test/dummy/config/initializers/secret_token.rb
+++ test/dummy/config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '4850c2e6bb07b55ed158e5d5e4211ea62a11a90c972200e0a5f08c46e89c4cbe3767a782a10116e64a3dcc512e69498ba7aa8857e699afc7ba8092c9d1b4f569'
+Dummy::Application.config.secret_token = 'ffdbff5ecd2a9b1be793644dc8ced0a952b8c89748cd9fc49f25a7055fd027d776f131c03de243abff3447716a6aeff340442e15e159e740d6505fe0964142dc'
+Dummy::Application.config.secret_key_base = '4850c2e6bb07b55ed158e5d5e4211ea62a11a90c972200e0a5f08c46e89c4cbe3767a782a10116e64a3dcc512e69498ba7aa8857e699afc7ba8092c9d1b4f569'
